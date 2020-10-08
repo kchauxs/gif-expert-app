a41533facebc6847239bfec3e2145fd4b1e8b683
kevin chaux <k55chasacch@gmail.com>
2020-10-08 18:51:29 -0500

    Agregando Docs

---
+++ docs/precache-manifest.751e8dd624a207e9c9269ca074d2517f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb34589bcc7e3a7495999aea5dff005f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd6b11ea907758688fc4",
+    "url": "/static/css/main.b4739b7c.chunk.css"
+  },
+  {
+    "revision": "af1aaa1542d3745c1513",
+    "url": "/static/js/2.90256544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90256544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6b11ea907758688fc4",
+    "url": "/static/js/main.7c902eda.chunk.js"
+  },
+  {
+    "revision": "851e4c3c4aa894498d54",
+    "url": "/static/js/runtime-main.8ac25dc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
